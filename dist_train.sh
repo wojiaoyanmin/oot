bc8e95bb779a7c565f98b4c0be2210b32a655c73
wojiaoyanmin <358787362@qq.com>
2021-01-05 11:33:16 +0800

    0

--- dist_train.sh
+++ dist_train.sh
@@ -2,16 +2,28 @@
 
 PORT=${PORT:-29500}
 
-cd sharefeature_20
-echo "sharefeature_20 dcn_1x"
+cd share_feature_20_pascal
+echo "share_feature_20_pascal50"
 PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
-python -m torch.distributed.launch --nproc_per_node=2 --master_port=$PORT \
-    ./tools/train.py configs/solo/solo_r50_fpn_1x_dcn_MHP.py  --resume-from work_dirs/solo_r50_fpn_1x_dcn_MHP/latest.pth --no-validate --launcher pytorch
+python -m torch.distributed.launch --nproc_per_node=1 --master_port=$PORT \
+    ./tools/train.py configs/solo/solo_r50_fpn_1x_dcn_PAS.py  --resume-from work_dirs/solo_r50_fpn_1x_dcn_PAS/latest.pth --no-validate --launcher pytorch
 
 
 sleep 60s
-echo "sharefeature_20 dcn_3x"
+echo "share_feature_20_pascal50-validate"
 PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
-python -m torch.distributed.launch --nproc_per_node=2 --master_port=$PORT \
-     ./tools/train.py configs/solo/solo_r50_fpn_3x_dcn_MHP.py --resume-from work_dirs/solo_r50_fpn_3x_dcn_MHP/epoch_9.pth --no-validate --launcher pytorch
+python -m torch.distributed.launch --nproc_per_node=1 --master_port=$PORT \
+     ./tools/test.py configs/solo/solo_r50_fpn_1x_dcn_PAS.py work_dirs/solo_r50_fpn_1x_dcn_PAS/latest.pth --eval segm --launcher pytorch
+
+sleep 60s
+echo "share_feature_20_pascal101"
+PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
+python -m torch.distributed.launch --nproc_per_node=1 --master_port=$PORT \
+    ./tools/train.py configs/solo/solo_r101_fpn_1x_dcn_PAS.py --resume-from work_dirs/solo_r101_fpn_1x_dcn_PAS/latest.pth --no-validate --launcher pytorch
+
+sleep 60s
+echo "share_feature_20_pascal101-validate"
+PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
+python -m torch.distributed.launch --nproc_per_node=1 --master_port=$PORT \
+     ./tools/test.py configs/solo/solo_r101_fpn_1x_dcn_PAS.py work_dirs/solo_r101_fpn_1x_dcn_PAS/latest.pth --eval segm --launcher pytorch
 
