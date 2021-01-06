512c17984cf436ce55f87fe74f3b514642a0f08b
wojiaoyanmin <358787362@qq.com>
2021-01-06 21:07:21 +0800

    0

--- dist_train.sh
+++ dist_train.sh
@@ -19,12 +19,21 @@ cd share_feature_20_pascal
 
 # sleep 60s
 echo "share_feature_20_pretrain"
+# PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
+# python -m torch.distributed.launch --nproc_per_node=1 --master_port=$PORT \
+#     ./tools/train.py configs/solo/solo_r50_fpn_1x_dcn_prePAS.py --launcher pytorch
+
+echo "share_feature_20_3X_TEST"
 PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
 python -m torch.distributed.launch --nproc_per_node=1 --master_port=$PORT \
-    ./tools/train.py configs/solo/solo_r50_fpn_1x_dcn_prePAS.py --launcher pytorch
-
+    ./tools/test.py configs/solo/solo_r50_fpn_3x_dcn_MHP.py work_dirs/solo_r50_fpn_3x_dcn_MHP/epoch_36.pth --eval segm --launcher pytorch
 sleep 60s
 echo "share_feature_20_pascal101"
 PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
 python -m torch.distributed.launch --nproc_per_node=1 --master_port=$PORT \
     ./tools/train.py configs/solo/solo_r101_fpn_1x_dcn_PAS.py --resume-from work_dirs/solo_r101_fpn_1x_dcn_PAS/latest.pth --no-validate --launcher pytorch
+sleep 60s
+echo "share_feature_20_pas_TEST"
+PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
+python -m torch.distributed.launch --nproc_per_node=1 --master_port=$PORT \
+    ./tools/test.py configs/solo/solo_r101_fpn_1x_dcn_PAS.py work_dirs/solo_r101_fpn_1x_dcn_PAS/latest.pth --eval segm --launcher pytorch
