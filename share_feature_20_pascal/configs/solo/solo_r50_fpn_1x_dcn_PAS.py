d9d7e39131a03e1ef8ad8ca8940fb9ebab33bfe2
wojiaoyanmin <358787362@qq.com>
2021-01-09 10:45:30 +0800

    0

--- share_feature_20_pascal/configs/solo/solo_r50_fpn_1x_dcn_PAS.py
+++ share_feature_20_pascal/configs/solo/solo_r50_fpn_1x_dcn_PAS.py
@@ -10,10 +10,10 @@ model=dict(
 data = dict(
     samples_per_gpu=2)
 optimizer = dict(type='SGD', lr=0.002, momentum=0.9, weight_decay=0.0001)
-total_epochs = 45
+total_epochs = 60
 lr_config = dict(
     policy='step',
     warmup='linear',
     warmup_iters=500,
     warmup_ratio=0.01,
-    step=[34,41])
+    step=[45,55])
